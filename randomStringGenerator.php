9e505a95bb71a4bbd75906c19030e4defca505f7
Rod Mcnew <rmcnew@relivinc.com>
2012-11-01 12:03:45 -0500

    add random string generator

---
+++ randomStringGenerator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Generates and random string consisting of numbers and uppercase letters
+ */
+for ($i = 1; $i <= 100; $i++) {
+    echo randomString(6) . '<br>';
+}
+
+function randomString(
+    $length,
+    $allowedCharacters = '123456789ABCDEFGHIJKLMNPQRSTUVWXYZ'//No 0's or O's
+)
+{
+    $maxIndex = strlen($allowedCharacters)-1;
+    $string = '';
+    for ($i = 1; $i <= $length; $i++) {
+        $string .= $allowedCharacters[mt_rand(0, $maxIndex)];
+    }
+    return $string;
+}
