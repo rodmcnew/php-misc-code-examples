b409231a1f38b26307dfe34a2ada3f43a6b868fa
Rod McNew <rmcnew@relivinc.com>
2015-01-19 10:48:11 -0600

    add file

---
+++ execShellCommandOverAndOverAndTimeIt.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Executes a shell command over and over and prints how long it took
+ *
+ * @author    Rod Mcnew
+ * @license   License.txt New BSD License
+ */
+$command = 'curl https://base.reliv.com -s';
+echo $command . "\n";
+$i = 0;
+while (true) {
+    $startTime = microtime(true);
+    $i++;
+    exec($command);
+    echo $i . ")" . number_format(
+            (microtime(true) - $startTime)
+            * 1000,
+            1
+        ) . "ms\n";
+}
